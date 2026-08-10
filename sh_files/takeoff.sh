6ac3b3557a34a4df7c7442b90287df947562a900
Mufafafa <2314774985@qq.com>
2026-08-10 19:21:46 +0800

    updata

--- sh_files/takeoff.sh
+++ sh_files/takeoff.sh
@@ -7,11 +7,7 @@ PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
 OPEN_DIR="$PROJECT_DIR/open"
 SETUP_FILE="$OPEN_DIR/devel/setup.zsh"
 
-if [ ! -f "$SETUP_FILE" ]; then
-    echo "[错误] 未找到开源工作空间环境文件: $SETUP_FILE"
-    echo "       请先编译 open 工作空间。"
-    exit 1
-fi
+
 source /opt/ros/noetic/setup.zsh
 source "$SETUP_FILE"
 
